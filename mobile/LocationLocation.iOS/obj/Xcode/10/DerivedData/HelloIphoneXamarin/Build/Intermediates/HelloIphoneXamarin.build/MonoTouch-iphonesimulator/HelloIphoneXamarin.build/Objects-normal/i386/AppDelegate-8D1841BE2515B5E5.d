68cf2282e9c6b642f9af0b0a70caaaf575a41d66
Robert Herber <herber88@gmail.com>
2013-09-20 10:45:24 +0200

    Removed crap

--- mobile/LocationLocation.iOS/obj/Xcode/10/DerivedData/HelloIphoneXamarin/Build/Intermediates/HelloIphoneXamarin.build/MonoTouch-iphonesimulator/HelloIphoneXamarin.build/Objects-normal/i386/AppDelegate-8D1841BE2515B5E5.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Robert/Projects/HelloIphoneXamarin/HelloIphoneXamarin/obj/Xcode/10/AppDelegate.m \
-  /Users/Robert/Projects/HelloIphoneXamarin/HelloIphoneXamarin/obj/Xcode/10/AppDelegate.h
--- mobile/LocationLocation.iOS/obj/Xcode/10/DerivedData/HelloIphoneXamarin/Build/Intermediates/HelloIphoneXamarin.build/MonoTouch-iphonesimulator/HelloIphoneXamarin.build/Objects-normal/i386/HelloIphoneXamarinViewController-21A11ADB2B2B04F8.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/Robert/Projects/HelloIphoneXamarin/HelloIphoneXamarin/obj/Xcode/10/HelloIphoneXamarinViewController.m \
-  /Users/Robert/Projects/HelloIphoneXamarin/HelloIphoneXamarin/obj/Xcode/10/HelloIphoneXamarinViewController.h
